f01ecdeb5806608100632b8cc9ce1d1214842f51
Daniel-Brestoiu <47580794+Daniel-Brestoiu@users.noreply.github.com>
2020-02-22 22:27:36 -0500

    Initial organizing thoughts Imported pyautogui

---
+++ Take_Screenshot.py
@@ -0,0 +1,18 @@
+import pyautogui
+
+"""
+General Process:
+
+Method A: To Screenshot
+
+    Simultaneous click Command-Control-Shift-4 to open general screen shot
+        - This causes mouse to become a top-left coordinate thing
+
+    Click Left
+    Move Mouse to bottom right corner of where I want (Still holding down left mouse)
+    Release left mouse click
+        - This takes the screenshot of the selected area.
+
+
+
+"""
